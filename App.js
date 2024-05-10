57e2edb864c3eefb450ccf432f37b808fe8ef90e
Abdullah Mehdi <theartistsam47@gmail.com>
2024-05-10 21:05:08 +0500

    App init

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+    return (
+        <View>
+            <Text>This is FoodHub</Text>
+        </View>
+    );
+};
+export default App;
